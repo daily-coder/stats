d8c2f0d011c580d0968db96c6c6bf0f71dfe77f9
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-03 17:47:57 +0530

    chore: update eslint for linting typescript files

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,9 +7,22 @@ module.exports = {
   extends: [
     "react-app",
     "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
     "plugin:import/recommended",
+    "plugin:import/typescript",
     "plugin:prettier/recommended",
   ],
+  settings: {
+    "import/extensions": [".js", ".jsx", ".ts", ".tsx"],
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      node: {
+        extensions: [".js", ".jsx", ".ts", ".tsx"],
+      },
+    },
+  },
   parserOptions: {
     ecmaFeatures: {
       jsx: true,
@@ -17,6 +30,22 @@ module.exports = {
     ecmaVersion: 12,
     sourceType: "module",
   },
+  overrides: [
+    {
+      files: ["**/*.ts?(x)"],
+      parser: "@typescript-eslint/parser",
+      parserOptions: {
+        ecmaVersion: 2018,
+        sourceType: "module",
+        ecmaFeatures: {
+          jsx: true,
+        },
+
+        warnOnUnsupportedTypeScriptVersion: true,
+      },
+      plugins: ["@typescript-eslint"],
+    },
+  ],
   plugins: ["react"],
   rules: {
     "no-console": "warn",
