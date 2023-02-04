c8dbd0751a525014631114ccaeab531359568896
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-04 20:47:14 +0530

    feat: migrate BgColor page to typescript

--- src/pages/BgColor.tsx
+++ src/pages/BgColor.tsx
@@ -1,12 +1,14 @@
 import BgCard from "../components/BgCard";
 import { useDataContext } from "../components/DataProvider";
 import WithLoading from "../components/WithLoading";
+import { cssData } from "../types";
 
 function BgColor() {
   const { cssData } = useDataContext();
+  // cssData is will never be null be because of HOC "WithLoading" function
   // "background-color" property points to array of background-color values.
-  const { "background-color": bgColors } =
-    cssData.stats.declarations.properties;
+  const { "background-color": bgColors } = (cssData as cssData).stats
+    .declarations.properties;
   const uniqueBgColors = [...new Set(bgColors)];
 
   const bgColorElements = uniqueBgColors.map((bgColor) => {
