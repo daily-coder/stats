1a2a2512699d857cef460409c8ef40ea18f410d8
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-04 20:46:15 +0530

    feat: migrate DataProvider component to typescript

--- src/components/DataProvider/DataProvider.tsx
+++ src/components/DataProvider/DataProvider.tsx
@@ -1,6 +1,19 @@
 import { createContext, useState, useContext } from "react";
 
-const DataContext = createContext(null);
+import type { cssData } from "../../types";
+
+interface DataContextType {
+  menuExpand: boolean;
+  setMenuExpand: React.Dispatch<React.SetStateAction<boolean>>;
+  cssData: cssData | null;
+  setCssData: React.Dispatch<React.SetStateAction<cssData | null>>;
+  loading: boolean;
+  setLoading: React.Dispatch<React.SetStateAction<boolean>>;
+  error: boolean;
+  setError: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+const DataContext = createContext<DataContextType | null>(null);
 
 export function useDataContext() {
   const context = useContext(DataContext);
@@ -10,11 +23,15 @@ export function useDataContext() {
   return context;
 }
 
-function DataProvider({ children }) {
+interface DataProviderProps {
+  children: React.ReactNode;
+}
+
+function DataProvider({ children }: DataProviderProps) {
   // On initial page load we will show menu if window width is greater than 768px and
   // hide it otherwise.
   const [menuExpand, setMenuExpand] = useState(() => window.innerWidth > 768);
-  const [cssData, setCssData] = useState(null);
+  const [cssData, setCssData] = useState<cssData | null>(null);
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(false);
 
