901c65e41bc3e593a1ce6786c755edac01c830a7
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-05 20:35:43 +0530

    fix: typo in import

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,12 +1,12 @@
-import Menubtn from "./MenuBtn";
-import Searchbar from "./SearchBar";
+import MenuBtn from "./MenuBtn";
+import SearchBar from "./SearchBar";
 import ToggleThemeBtn from "./ToggleThemeBtn";
 
 function Header() {
   return (
     <header className="flex h-24 items-center border-b border-slate-900/50 bg-white px-4 dark:border-slate-100/50 dark:bg-slate-900">
-      <Menubtn />
-      <Searchbar />
+      <MenuBtn />
+      <SearchBar />
       <ToggleThemeBtn />
     </header>
   );
