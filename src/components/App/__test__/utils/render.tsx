5733c47c3879d12f660ed3946e640713b2286755
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-06 19:45:10 +0530

    feat: migrate App component to typescript

--- src/components/App/__test__/utils/render.tsx
+++ src/components/App/__test__/utils/render.tsx
@@ -1,11 +1,12 @@
 import { render as rtlRender } from "@testing-library/react";
+import { ReactElement, ReactNode } from "react";
 import { HashRouter } from "react-router-dom";
 
-function render(ui, { route = "/", ...options } = {}) {
+function render(ui: ReactElement, { route = "/", ...options } = {}) {
   window.history.pushState({}, "Test page", route);
 
   // BrowserRouter is not supported in github pages
-  function Wrapper({ children }) {
+  function Wrapper({ children }: { children: ReactNode }) {
     return <HashRouter>{children}</HashRouter>;
   }
 
