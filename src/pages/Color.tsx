92de5385b340fc544803ea37842179bc28a9cc3b
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-06 19:26:20 +0530

    feat: migrate Color page to typescript

--- src/pages/Color.tsx
+++ src/pages/Color.tsx
@@ -1,11 +1,13 @@
 import ColorCard from "../components/ColorCard";
 import { useDataContext } from "../components/DataProvider";
 import WithLoading from "../components/WithLoading";
+import { cssData } from "../types";
 
 function Color() {
   const { cssData } = useDataContext();
+  // WithLoading prevents cssData as null.
   // color property points to array of color values.
-  const { color: colors } = cssData.stats.declarations.properties;
+  const { color: colors } = (cssData as cssData).stats.declarations.properties;
   const uniqueColors = [...new Set(colors)];
 
   const colorElements = uniqueColors.map((color) => {
