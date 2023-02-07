42c3dc75ead72cd1ad43406c5feac3d4caaec644
daily-coder <108210627+githubcoder999@users.noreply.github.com>
2023-02-07 20:06:14 +0530

    feat: migrate mocks to typescript

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,14 +1,15 @@
 import { rest } from "msw";
 
+import { hasOwnProperty } from "../typeUtils";
+
 import WEBSITE_STATS from "./website-stats";
 
 export const handlers = [
   rest.get("https://cssstats.com/api/stats", (req, res, ctx) => {
     const website = req.url.searchParams.get("url");
-    const data = WEBSITE_STATS[website];
-    if (data == null) {
+    if (website == null || !hasOwnProperty(WEBSITE_STATS, website)) {
       return res(ctx.status(400));
     }
-    return res(ctx.status(200), ctx.json(data));
+    return res(ctx.status(200), ctx.json(WEBSITE_STATS[website]));
   }),
 ];
